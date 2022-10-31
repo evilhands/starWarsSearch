2e88cf6cdf60f09ceb88810eb8846123479e8f8f
Philipp Schwarzmaier <pschwarzmaier@gmx.at>
2022-10-31 10:34:38 +0100

    add routing to peoplelist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeopleListComponent } from './people-list/people-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PeopleListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
